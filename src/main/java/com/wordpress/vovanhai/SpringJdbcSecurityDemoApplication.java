f90eb78a0fac2247952d38642f31f5ff5bcf8faf
Vo Van Hai <vovanhai@iuh.edu.vn>
2023-11-24 20:18:20 +0700

    acafaf

---
+++ src/main/java/com/wordpress/vovanhai/SpringJdbcSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wordpress.vovanhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcSecurityDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wordpress/vovanhai/SpringJdbcSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordpress.vovanhai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
